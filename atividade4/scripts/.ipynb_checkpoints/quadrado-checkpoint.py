1a2999dec7e37fe5e0e565212efdd810f3e0323c
caioesr <>
2020-10-20 17:37:39 -0300

    minha parte tá feita

---
+++ atividade4/scripts/.ipynb_checkpoints/quadrado-checkpoint.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist, Vector3
+import math
+import time
+
+v = 0.1  # Velocidade linear
+w = math.pi/2  # Velocidade angular
+
+if __name__ == "__main__":
+    rospy.init_node("roda_exemplo")
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size=3)
+
+    try:
+        i = 0
+        while not rospy.is_shutdown():
+            vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+            if i%2:
+                vel = Twist(Vector3(0,0,0), Vector3(0,0,1.5))
+            else:
+                vel = Twist(Vector3(1,0,0), Vector3(0,0,0))
+            pub.publish(vel)
+            i += 1
+            rospy.sleep(0.1)
+    except rospy.ROSInterruptException:
+        print("Ocorreu uma exceção com o rospy")
